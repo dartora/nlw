84ca0944011d16fec6303064577178131dfa92b3
rodolfo <rodolfo.ds@aluno.ifsc.edu.br>
2020-06-05 12:16:37 -0300

    aula 02 - stop almoco

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,20 @@
-import express from "express";
+import express, { request, response } from "express";
 
 const app = express();
-
+const users = ["Diego", "Clayton", "Robson", "1112"];
 app.get("/users", (request, response) => {
   console.log("Listagem de users");
-  response.json(["Diego", "Clayton", "Robson", "1112"]);
+  return response.json(users);
+});
+
+app.get("/users/:id", (request, response) => {
+  return response.json(users[Number(request.params.id)]);
+});
+
+app.post("/users", (request, response) => {
+  // console.log("Listagem de users");
+  const user = { name: "Rodolfo", email: "rodolfo.ds@aluno.ifsc.edu.br" };
+  return response.json(user);
 });
 
 app.listen(3333);
