5d5a97dea28d5c1d74ab5ee7e6064f777c9298a2
rodolfo <rodolfo.ds@aluno.ifsc.edu.br>
2020-07-01 16:12:49 -0300

    1:34:00

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,44 @@
-import express from "express";
+import express, { request, response } from "express";
+import knex from "./database/connection";
 const routes = express.Router();
 
-routes.get("/", (request, response) => {
-  return response.json({ nessage: "Hello sexta krl!" });
+routes.get("/items", async (request, response) => {
+  const items = await knex("items").select("*");
+
+  const serializedItems = items.map((item) => {
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/` + item.image,
+    };
+  });
+
+  return response.json(serializedItems);
 });
 
+routes.post("/points", async (request, response) => {
+  const { name, email, latitude, longitude, city, uf, items } = request.body;
+
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx("point").insert({
+    image: "image-fake",
+    name,
+    email,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+  const point_id = insertedIds[0];
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id,
+    };
+  });
+
+  await trx("point_items").insert(pointItems);
+  return response.json({ sucess: true });
+});
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express, { request, response } from "express";
 import routes from "./routes";
+import path from "path";
 
 const app = express();
 
@@ -7,4 +8,7 @@ const users = ["Diego", "Clayton", "Robson", "1112"];
 
 app.use(express.json());
 app.use(routes);
+
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+
 app.listen(3333);
